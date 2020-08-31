3163497917368b19bfccbeb24e79e1719b6b036b
jisu3426 <jishushu@naver.com>
2020-08-31 14:13:34 +0900

    add method (class C)

--- CoCoCoWork/src/com/kh/cococo/C.java
+++ CoCoCoWork/src/com/kh/cococo/C.java
@@ -1,15 +1,19 @@
 package com.kh.cococo;
 
 public class C {
-<<<<<<< HEAD
+
 	
 	public void test() {
 		
 	}
 
-=======
+
 	public void sujin() {
 		
 	}
->>>>>>> branch 'master' of https://github.com/sujin99/cowork.git
+	
+	public void jisu() {
+		
+	}
+
 }
